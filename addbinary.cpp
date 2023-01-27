3d3c72af1927072a2e701fc4d8d842169d2cd3d9
MD Abdullah AL Mamun <47145788+ronssrony@users.noreply.github.com>
2023-01-27 14:14:25 +0600

    Create addbinary.cpp

---
+++ addbinary.cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+using namespace std;
+int addbinary(int a , int b) 
+  
+  {
+       int ans = 0 ;
+       int prevcarry = 0 ; 
+       while(a && b)
+       {
+          if(a%2==0 && b%2==0)
+       {   
+           ans = ans*10 + prevcarry ; 
+           prevcarry = 0 ; 
+          
+       }
+       else if ((a%2==0 && b%2==1)|| ((a%2==1 && b%2==0)))
+       {
+           if(prevcarry==0)
+           {
+            ans = ans*10 + 1 ;
+            prevcarry  = 0 ;
+           }
+           else 
+           {
+              ans = ans*10 + 0 ;
+              prevcarry = 1 ;
+           }
+       }
+       else 
+       {
+          ans = ans*10 + prevcarry ;
+          prevcarry = 1 ;
+       }
+       a/= 10 ;
+       b /=10 ;
+       }
+       while(a)
+       {
+          if(prevcarry==1)
+          {
+            if(a%2==1)
+            {
+                ans = ans*10 + 0 ;
+                prevcarry = 1; 
+            }
+            else
+            {
+                ans = ans*10 +1 ;
+                prevcarry  = 0 ;
+            }
+          }
+          else 
+          {
+            ans = ans*10+(a%2) ;
+           
+          }
+          a /=10 ;
+       }
+         while(b)
+       {
+          if(prevcarry==1)
+          {
+            if(b%2==1)
+            {
+                ans = ans*10 + 0 ;
+                prevcarry = 1; 
+            }
+            else
+            {
+                ans = ans*10 +1 ;
+                prevcarry  = 0 ;
+            }
+          }
+          else 
+          {
+            ans = ans*10+(b%2) ;
+           
+          }
+          b /=10 ;
+       }
+       if(prevcarry==1)
+       {
+        ans = ans*10+1 ;
+       }
+    
+
+       return ans ;
+  }
+   int rev(int n)
+     {
+        int res ;
+        while(n)
+        {
+         res = res*10 + n%10 ;
+       n /=10 ;
+        }
+        
+        return res ;
+
+     }
+int main()
+{
+   
+   int a , b ; 
+   cin>>a>>b ;
+   cout<<rev(addbinary(a,b))<<endl;
+   
+    
+    return 0 ;
+}
